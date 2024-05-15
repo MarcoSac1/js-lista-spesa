062909c4076b4d595686196be6267bbd37868c16
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-15 15:58:40 +0200

    styled css , created cicle for reference and selected element inside DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-const spesaArray =[pane, pasta, frutta, cocacola, noci, carne, insalata, shampo];
+const spesaArray =[ 'pane', 'pasta', 'frutta', 'cocacola', 'noci', 'carne', 'insalata', 'shampo'];
+// console.log(spesaArray)
+
+// seleziono il primo ed unico ul al inteno del DOM
+const ulEL = document.querySelector('ul');
+
+// creo un ciclo while
+for (let index = 0; index < spesaArray.length; index++) {
+    
+    // creo un elemento al interno del DOM
+    const liEl = document.createElement('li');
+
+    liEl.append(spesaArray[index]);
+
+    ulEL.appendChild(liEl);
+    
+}
